ea82be17190e3778add447c5c24cbcc19eb1522c
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-05-06 22:05:25 -0300

    traduzindo o projeto

--- src/main/java/org/example/GameLogic.java
+++ src/main/java/org/example/GameLogic.java
@@ -9,6 +9,7 @@ public class GameLogic {
     private static boolean playing = true;
     private static boolean forceUntilCorrect = false;
     private static int score;
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
 
     public static void operateOption(String option) {
         while (playing) {
@@ -25,19 +26,19 @@ public class GameLogic {
     }
 
     private static void checkScore() {
-        System.out.println("Your current score is: " + score);
+        System.out.println(bundle.getString("current.score") + score);
         Menu.menu();
     }
 
     private static void resetScore() {
         score = 0;
-        System.out.println("Your score is now set to: " + score);
+        System.out.println(bundle.getString("reset.score") + score);
         Menu.menu();
     }
 
     public static void play() {
         if (table.isEmpty()) {
-            System.out.println("You cant play with an empty preset! Please select or create a preset");
+            System.out.println(bundle.getString("empty.warning"));
             presets();
             return;
         }
@@ -52,17 +53,17 @@ public class GameLogic {
             String userAnswer = scanner.nextLine();
 
             if (!isValidNumber(userAnswer)) {
-                System.out.println("Your final score: " + score);
+                System.out.println(bundle.getString("final.score") + score);
                 Menu.menu();
                 return;
             }
 
             if (userAnswer.equals(String.valueOf(correctAnswer))) {
                 score++;
-                System.out.println("Correct!");
+                System.out.println(bundle.getString("correct"));
                 break;
             } else {
-                System.out.println("Wrong!");
+                System.out.println(bundle.getString("wrong"));
                 if (!forceUntilCorrect) {
                     break;
                 }
@@ -71,17 +72,9 @@ public class GameLogic {
     }
 
     public static void configure() {
-        System.out.println("""
-            =========================================
-                            Configure
-            =========================================
-            1. Presets
-            2. Add number in multiplication table (one at time)
-            3. Remove number in multiplication table (one at time)
-            4. Toggle "Answer until correct" (Currently: %s)
-            5. Return menu
-            """.formatted(forceUntilCorrect ? "TRUE" : "FALSE"));
-        System.out.print("Type your option here: ");
+        System.out.println(bundle.getString("configure.text").formatted(forceUntilCorrect
+                ? bundle.getString("forceUntilCorrect.mode.on") : bundle.getString("forceUntilCorrect.mode.off")));
+        System.out.print(bundle.getString("configure.prompt"));
         String option = scanner.nextLine();
 
         switch (option) {
@@ -95,7 +88,8 @@ public class GameLogic {
 
     private static void toggleAnswerUntilCorrect() {
         forceUntilCorrect = !forceUntilCorrect;
-        System.out.println("Answer Until Correct is now set to: " + (forceUntilCorrect ? "TRUE" : "FALSE"));
+        System.out.println(bundle.getString("forceUntilCorrect.mode.changed") + (forceUntilCorrect
+                ? bundle.getString("forceUntilCorrect.mode.on") : bundle.getString("forceUntilCorrect.mode.off")));
         Menu.menu();
     }
 
@@ -104,8 +98,8 @@ public class GameLogic {
 
         String message = operation.getMessage();
 
-        System.out.printf("The current multiplication table is: %s%n%n", table);
-        System.out.printf("Type here the number you want to %s (1 to 10) or any letter to return menu: ", message);
+        System.out.printf(bundle.getString("current.table"), table);
+        System.out.printf(bundle.getString("change.table.prompt"), message);
         String userInput = scanner.nextLine();
 
         if (!isValidNumber(userInput)) {
@@ -116,58 +110,52 @@ public class GameLogic {
         int number = Integer.parseInt(userInput);
 
         if (number < 1 || number > 10) {
-            System.out.println("Number must be between 1 and 10");
+            System.out.println(bundle.getString("number.out.of.range"));
             returnToMenuChangeTable(operation);
             return;
         }
 
         if (isEqualsAdd ? table.add(number) : table.remove(number)) {
-            System.out.printf("Number '%d' successfully %s.%n", number, message);
+            String messagePast = isEqualsAdd
+                    ? bundle.getString("add.past.version")
+                    : bundle.getString("remove.past.version");
+
+            System.out.printf(bundle.getString("number.successfully.operation"), number, messagePast);
         } else {
-            System.out.printf("You can't %s number '%d'. It is %s in the table.%n",
-                    message, number, isEqualsAdd ? "already present" : "absent");
+            System.out.printf(bundle.getString("number.error.message"),
+                    message, number, isEqualsAdd ? bundle.getString("number.already.present")
+                            : bundle.getString("number.absent"));
         }
 
         returnToMenuChangeTable(operation);
     }
 
     private static void presets() {
-        System.out.println("""
-                ===================================
-                              Presets
-                ===================================
-                1. Include only [6, 7, 8, 9]
-                2. Include only [3, 4, 6, 7, 8, 9]
-                3. Include only [2, 3, 4, 5, 6, 7, 8, 9]
-                4. Include all [1 to 10]
-                5. Check current preset
-                6. Create custom preset
-                7. Return menu
-                """);
+        System.out.println(bundle.getString("presets.text"));
 
-        System.out.print("Type your option here: ");
+        System.out.print(bundle.getString("presets.prompt"));
         String option = scanner.nextLine();
 
         switch (option) {
             case "1" -> {
                 table.clear();
                 table.addAll(Set.of(6, 7, 8, 9));
-                System.out.println("Preset 1 applied.");
+                System.out.printf(bundle.getString("presets.applied"), option);
             }
             case "2" -> {
                 table.clear();
                 table.addAll(Set.of(3, 4, 6, 7, 8, 9));
-                System.out.println("Preset 2 applied.");
+                System.out.printf(bundle.getString("presets.applied"), option);
             }
             case "3" -> {
                 table.clear();
                 table.addAll(Set.of(2, 3, 4, 5, 6, 7, 8, 9));
-                System.out.println("Preset 3 applied.");
+                System.out.printf(bundle.getString("presets.applied"), option);
             }
             case "4" -> {
                 table.clear();
                 table.addAll(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-                System.out.println("Preset 4 applied.");
+                System.out.printf(bundle.getString("presets.applied"), option);
             }
             case "5" -> {
                 checkCurrentPreset();
@@ -183,27 +171,20 @@ public class GameLogic {
             }
         }
 
-        System.out.printf("Updated table: %s%n", table);
+        System.out.printf(bundle.getString("updated.table"), table);
     }
 
     private static void checkCurrentPreset() {
-        System.out.printf("Current preset: %s%n", table);
+        System.out.printf(bundle.getString("current.preset"), table);
     }
 
     private static void createCustomPreset() {
-        System.out.println("""
-            =============================================================
-                                    Custom Preset
-            =============================================================
-            Type the numbers you want to include in your preset (1 to 10),
-            separated by spaces (ex: '1 2 6 10' or '4 9' or '5'):
-            Type 'done' when you finish.
-            """);
+        System.out.println(bundle.getString("custom.preset.text"));
 
         Set<Integer> customPreset = new HashSet<>();
 
         while (true) {
-            System.out.print("Enter numbers or 'done' to finish: ");
+            System.out.print(bundle.getString("preset.custom.prompt"));
             String userInput = scanner.nextLine().trim();
 
             if (userInput.equalsIgnoreCase("done")) {
@@ -216,31 +197,31 @@ public class GameLogic {
                 try {
                     int number = Integer.parseInt(userNumber.trim());
                     if (number < 1 || number > 10) {
-                        System.out.println("Number must be between 1 and 10. Please try again.");
+                        System.out.println(bundle.getString("number.out.of.range"));
                         break;
                     }
                     customPreset.add(number);
                 } catch (NumberFormatException e) {
-                    System.out.println("Invalid input, please enter only numbers separated by spaces.");
+                    System.out.println(bundle.getString("preset.custom.invalid"));
                     break;
                 }
             }
         }
 
         if (customPreset.isEmpty()) {
-            System.out.println("No valid numbers entered. Previous preset was kept: " + table);
+            System.out.println(bundle.getString("preset.custom.empty") + table);
             return;
         }
 
         table.clear();
         table.addAll(customPreset);
 
-        System.out.printf("Custom preset applied. Updated table: %s%n", table);
+        System.out.printf(bundle.getString("preset.custom.success"), table);
     }
 
     private enum Operation {
-        ADD("add"),
-        REMOVE("remove");
+        ADD(bundle.getString("add")),
+        REMOVE(bundle.getString("remove"));
 
         private final String message;
 
@@ -254,8 +235,8 @@ public class GameLogic {
     }
 
     private static void returnToMenuChangeTable(Operation operation) {
-        System.out.printf("Updated table: %s%n%n", table);
-        System.out.print("Return to menu? (Y/N) ");
+        System.out.printf(bundle.getString("updated.table"), table);
+        System.out.print(bundle.getString("return.menu"));
         String userAnswer = scanner.nextLine().trim();
 
         if (userAnswer.equalsIgnoreCase("y")) {
--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,22 +1,15 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Menu {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
 
     public static void menu() {
-        System.out.println("""
-        =======================================================================
-        Welcome to Command Line Interface game to memorize multiplication table
-        =======================================================================
-        1. Play
-        2. Configure game
-        3. Check current score
-        4. Reset score
-        5. Leave (You can also type any letter)
-        """);
-        System.out.print("Type your option here: ");
+        System.out.print(bundle.getString("menu.text"));
 
         String option = scanner.nextLine();
         GameLogic.operateOption(option);
